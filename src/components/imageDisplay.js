86a0232d59ef8a668457b6d72d4ab162dc06e8c9
Akib-Raihan-4 <raihanakib062@gmail.com>
2023-09-15 18:20:54 -0400

    completed

--- src/components/imageDisplay.js
+++ src/components/imageDisplay.js
@@ -11,7 +11,7 @@ export const ImageDisplay = ({imageUrl}) => {
 
     const [copied, setCopied] = useState(false)
   return (
-    <div className="sm:w-[552px] sm:h-[669px] w-[350px] bg-white rounded-[12px] shadow-[0px_0px_40px_#0000001a]">
+    <div className="sm:w-[552px] sm:h-[669px] w-[350px] h-[400px] bg-white rounded-[12px] shadow-[0px_0px_40px_#0000001a]">
         <div className={`flex flex-col items-center h-full 
              rounded-[20px]  sm:gap-[20px]`}
             >
@@ -19,12 +19,14 @@ export const ImageDisplay = ({imageUrl}) => {
                 <h1 className="[font-family:'Poppins-Medium',Helvetica] font-medium text-[#4f4f4f] sm:text-[25px]">Uploaded successfully</h1>
 
                 <img src={imageUrl} className="sm:h-[300px] h-[200px] sm:w-[500px] w-[330px] sm:m-[25px] m-[10px] rounded-[20px]" />
-                <div className='flex justify-evenly sm:h-[44px] sm:w-[500px] border bg-[#f6f8fb] rounded-[12px] border-solid border-[#e0e0e0]'>
-                    <div className="[font-family:'Poppins-Medium',Helvetica] font-medium text-[#4f4f4f] text-[7px] flex items-center">
-                        {imageUrl}
+
+                <div className='flex justify-evenly sm:h-[44px] h-[48px] sm:w-[500px] w-[330px] border bg-[#f6f8fb] rounded-[12px] border-solid border-[#e0e0e0]'>
+                    <div className="[font-family:'Poppins-Medium',Helvetica] font-medium text-[#4f4f4f] sm:text-[7.1px] text-[6px]  ml-1  flex items-center ">
+                        {/* {imageUrl} */}
+                        https://huqxornsvjuktkebfjzd.supabase.co/storage/v1/object/public/images/images/61029465-0d1c-4d12-9b04-a963ec64dd03
                     </div>
                     <button
-                        className="bg-[#2f80ed] rounded-[8px] [font-family:'Poppins-Medium',Helvetica] font-medium text-white px-[15px] my-[1.5px] hover:opacity-80"
+                        className="bg-[#2f80ed] rounded-[8px] [font-family:'Poppins-Medium',Helvetica] font-medium sm:text-[14px] text-[8px] text-white sm:mr-0 mr-[2px] px-[15px] my-[1.5px]  hover:opacity-80"
                         onClick={handleCopy}
                     >
                         {copied ? 'Copied!' : 'Copy Link'}
